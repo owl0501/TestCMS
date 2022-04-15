bdce7d7dad39d703b1f346a65f7da8275e8d8c24
Johnny <johnnychen@changingtec.com>
2022-04-15 15:37:28 +0800

    [add] DBfirst 建立 Entity

---
+++ TestCMS.Entity/Entity/CMSDBContext.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace TestCMS.Entity.Entity
+{
+    public partial class CMSDBContext : DbContext
+    {
+        public CMSDBContext()
+        {
+        }
+
+        public CMSDBContext(DbContextOptions<CMSDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<OrderList> OrderLists { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=127.0.0.1;Database=CMSDB;Persist Security Info=True;User ID=sa;Password=Pw0922435545");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Chinese_Taiwan_Stroke_CI_AS");
+
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.ToTable("Category");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<OrderList>(entity =>
+            {
+                entity.ToTable("OrderList");
+
+                entity.Property(e => e.Id)
+                    .HasMaxLength(15)
+                    .IsUnicode(false)
+                    .IsFixedLength(true);
+
+                entity.Property(e => e.ProductId).HasColumnName("Product_id");
+
+                entity.Property(e => e.Stock).HasColumnName("stock");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.OrderLists)
+                    .HasForeignKey(d => d.ProductId)
+                    .HasConstraintName("FK__OrderList__Produ__44FF419A");
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.ToTable("Product");
+
+                entity.Property(e => e.Image)
+                    .HasMaxLength(300)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Intro)
+                    .HasMaxLength(1000)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ReleaseDate).HasColumnType("date");
+
+                entity.HasOne(d => d.Category)
+                    .WithMany(p => p.Products)
+                    .HasForeignKey(d => d.CategoryId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Product__Categor__45F365D3");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ TestCMS.Entity/Entity/OrderList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace TestCMS.Entity.Entity
+{
+    public partial class OrderList
+    {
+        public string Id { get; set; }
+        public int? ProductId { get; set; }
+        public int? Stock { get; set; }
+
+        public virtual Product Product { get; set; }
+    }
+}
