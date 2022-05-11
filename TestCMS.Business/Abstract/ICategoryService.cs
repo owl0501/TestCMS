1af503661badd402c5344f718275e64322d0e9bb
JohnnyChen <johnnychen@changingtec.com>
2022-05-12 00:11:39 +0800

    [add] 新增類別服務

---
+++ TestCMS.Business/Abstract/ICategoryService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TestCMS.Entity.Entity;
+namespace TestCMS.Business.Abstract
+{
+    public interface ICategoryService
+    {
+        /// <summary>
+        /// 查詢所有資料
+        /// </summary>
+        /// <returns></returns>
+        Task<IList<CategoryTable>> Get();
+
+        /// <summary>
+        /// 新增資料
+        /// </summary>
+        /// <param name="category"></param>
+        /// <returns></returns>
+        Task<CategoryTable> CreateCategory(CategoryTable category);
+
+        /// <summary>
+        /// 刪除資料
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task Delete(int id);
+    }
+}
---
+++ TestCMS.Business/Concrete/CategoryService.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TestCMS.Business.Abstract;
+using TestCMS.DataAccess.Abstract;
+using TestCMS.Entity.Entity;
+
+namespace TestCMS.Business.Concrete
+{
+    public class CategoryService : ICategoryService
+    {
+        /// <summary>
+        /// 建構子DI
+        /// </summary>
+        private readonly ICategoryRepo _repo;
+        public CategoryService(IServiceProvider provider)
+        {
+            _repo = provider.GetRequiredService<ICategoryRepo>();
+        }
+        /// <summary>
+        /// 新增類別
+        /// </summary>
+        /// <param name="category"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task<CategoryTable> CreateCategory(CategoryTable category)
+        {
+            return _repo.Create(category);
+        }
+        /// <summary>
+        /// 刪除類別
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task Delete(int id)
+        {
+            return _repo.Delete(id);
+        }
+
+        /// <summary>
+        /// 查詢所有類別
+        /// </summary>
+        /// <returns></returns>
+
+        public Task<IList<CategoryTable>> Get()
+        {
+            return _repo.GetAsync();
+        }
+    }
+}
