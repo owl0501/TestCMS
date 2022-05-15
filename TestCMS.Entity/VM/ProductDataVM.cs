3df763b266f00e7f18ce65ae1721d040525dd4f1
JohnnyChen <johnnychen@changingtec.com>
2022-05-15 18:02:37 +0800

    [add] 新增傳給首頁的VM

---
+++ TestCMS.Entity/VM/ProductDataVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestCMS.Entity.VM
+{
+    public class ProductDataVM
+    {
+        public string ImagePath { get; set; }
+        public string  Name { get; set; }
+        public string Category { get; set; }
+        public string  Intro { get; set; }
+        public string SupplyState { get; set; }
+        public bool IsNew { get; set; }
+    }
+}
