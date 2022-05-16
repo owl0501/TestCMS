fc1ee3dca1955d96daedbd77feed5e2d4d900af7
Johnny <johnnychen@changingtec.com>
2022-05-16 18:28:53 +0800

    [modify] 為調整

--- TestCMS.Entity/VM/ProductDataVM.cs
+++ TestCMS.Entity/VM/ProductDataVM.cs
@@ -6,6 +6,7 @@ namespace TestCMS.Entity.VM
 {
     public class ProductDataVM
     {
+        public int Id { get; set; }
         public string ImagePath { get; set; }
         public string  Name { get; set; }
         public string Category { get; set; }
