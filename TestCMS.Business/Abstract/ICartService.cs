6216cfbd7f081e07410ef407af508435317a7bcb
JohnnyChen <johnnychen@changingtec.com>
2022-05-22 15:42:35 +0800

    [add] 新增CartService

---
+++ TestCMS.Business/Abstract/ICartService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TestCMS.Entity.Entity;
+
+namespace TestCMS.Business.Abstract
+{
+    public interface ICartService
+    {
+        /// <summary>
+        /// 查詢所有項目
+        /// </summary>
+        /// <returns></returns>
+        Task<IList<CartTable>> Get();
+        /// <summary>
+        /// 新增資料
+        /// </summary>
+        /// <param name="category"></param>
+        /// <returns></returns>
+        bool CartAdd(CartTable cart);
+        /// <summary>
+        /// 刪除資料
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task Delete(int id);
+        /// <summary>
+        /// 更新資料
+        /// </summary>
+        /// <param name="cart"></param>
+        /// <returns></returns>
+        Task Update(CartTable cart);
+    }
+}
---
+++ TestCMS.Business/Concrete/CartService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TestCMS.Business.Abstract;
+using TestCMS.Entity.Entity;
+using TestCMS.DataAccess.Abstract;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TestCMS.Business.Concrete
+{
+    public class CartService : ICartService
+    {
+        /// <summary>
+        /// 建構子DI
+        /// </summary>
+        private readonly ICartRepo _repo;
+        public CartService(IServiceProvider provider)
+        {
+            _repo = provider.GetRequiredService<ICartRepo>();
+        }
+
+        public bool CartAdd(CartTable cart)
+        {
+            bool isOK = true;
+            try
+            {
+                //若存在修改+1,否則新增
+                var data = _repo.Get(cart.Id);
+                if (data != null)
+                {
+                    _repo.Update(cart);
+                }
+                else
+                {
+                    _repo.Create(cart);
+                }
+            }
+            catch(Exception ex)
+            {
+                isOK = false;
+                throw ex;
+            }
+            
+            return isOK;
+        }
+
+        public Task Delete(int id)
+        {
+            return _repo.Delete(id);
+        }
+
+        public Task<IList<CartTable>> Get()
+        {
+            return _repo.Get();
+        }
+
+        public Task Update(CartTable cart)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
