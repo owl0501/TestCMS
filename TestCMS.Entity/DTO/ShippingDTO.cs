80253e84461ceadf4aa770e02e619d5c0e484558
JohnnyChen <johnnychen@changingtec.com>
2022-06-12 16:02:57 +0800

    [add] 新增出貨DTO(傳後端資料)

---
+++ TestCMS.Entity/DTO/ShippingDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestCMS.Entity.DTO
+{
+    public class ShippingDTO
+    {
+        public IList<int> CartIdList { get; set; } 
+        public int CategoryId { get; set; }
+    }
+}
