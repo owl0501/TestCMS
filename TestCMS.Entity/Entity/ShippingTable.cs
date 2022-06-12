ec57a1c3019f70fa34112edea02b331c95a5975f
JohnnyChen <johnnychen@changingtec.com>
2022-06-12 16:02:28 +0800

    [modify] 出貨資料表

--- TestCMS.Entity/Entity/ShippingTable.cs
+++ TestCMS.Entity/Entity/ShippingTable.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -8,8 +9,11 @@ namespace TestCMS.Entity.Entity
     public partial class ShippingTable
     {
         public int Id { get; set; }
+        [Display(Name ="出貨編號")]
         public string ShipId { get; set; }
+        [Display(Name = "產品類型")]
         public int CategoryId { get; set; }
+        [Display(Name = "建立日期")]
         public DateTime CreateTime { get; set; }
 
         public virtual CategoryTable Category { get; set; }
