793d8746afdec46d11b65809b045b0d097bf8f4d
JohnnyChen <johnnychen@changingtec.com>
2022-06-15 22:06:36 +0800

    [add] Add ErrorController and View

---
+++ TsetCMS.Web/Controllers/ErrorController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TestCMS.Helper.Data;
+
+namespace TsetCMS.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        /// <summary>
+        /// Internal Server Error
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Error()
+        {
+            return View(new ErrorVM { RequestId = Activity.Current.Id });
+        }
+    }
+}
